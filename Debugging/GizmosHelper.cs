66ef12b50c47704cae283d54a6c0b514d9c2fe99
Shariar Papon <spaponwork@gmail.com>
2025-08-21 01:36:09 -0400

    GizmosHelper updated

--- Debugging/GizmosHelper.cs
+++ Debugging/GizmosHelper.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 using UnityEditor;
 using UnityEngine;
 
@@ -22,6 +23,7 @@ namespace SPToolkits.Debugging
             }
         }
 
+        private static Dictionary<string, object> _KeyValues = new Dictionary<string, object>();
         private Dictionary<string, Action> _drawCalls = new Dictionary<string, Action>();
         private List<string> _ids = new List<string>();
 
@@ -44,6 +46,51 @@ namespace SPToolkits.Debugging
             => GetStyle(FontStyle.Normal, fontColor, 14);
 
         /// <summary>
+        /// Creates or updated the value with the given key.
+        /// </summary>
+        /// <remarks>
+        /// The value can later be retrived by the key from elsewhere. 
+        /// <br>This allows data to be shared between multiple objects using GizmosHelper.</br>
+        /// </remarks>
+        public static void SetKeyValue(string key, object value) 
+        {
+            if (_KeyValues.ContainsKey(key))
+                _KeyValues[key] = value;
+            else
+                _KeyValues.Add(key, value);
+        }
+
+        /// <returns>True, if a value was found with the key.</returns>
+        public static bool TryGetValue<T>(string key, out T value) 
+        {
+            value = default;
+            if (_KeyValues.TryGetValue(key, out var obj)) 
+            {
+                value = (T)obj;
+                return true;
+            }
+            return false;
+        }
+
+        /// <returns>True, if a value was found with the key.</returns>
+        public bool TryGetValueRaw(string key, out object value)
+        {
+            return _KeyValues.TryGetValue(key, out value);
+        }
+
+        /// <summary>
+        /// Prints the key value pairs to the console.
+        /// </summary>
+        public static void PrintKeyValues() 
+        {
+            StringBuilder buffer = new StringBuilder();
+            buffer.AppendLine("<color=yellow>GIZMOS HELPER KEY-VALUES</color>");
+            foreach (var kvp in _KeyValues)
+                buffer.AppendLine($"<color=orange>{kvp.Key}</color> | <color=cyan>{kvp.Value}</color>");
+            Debug.Log(buffer.ToString());
+        }
+
+        /// <summary>
         /// <br>Registers the draw action to be called within OnDrawGizmos.
         /// <br/>If one already exists with the same id, it will be overwritten.</br>
         /// </summary>
@@ -56,6 +103,16 @@ namespace SPToolkits.Debugging
             }
             else
                 Instance._drawCalls[id] = drawAction;
+        }
+
+        /// <returns>Random color generated with the given seed.</returns>
+        public static Color RandomColor(string seed) 
+        {
+            System.Random prng = new System.Random(seed.GetHashCode());
+            float r = (float)(prng.NextDouble() * 255d) / 255f, 
+                  g = (float)(prng.NextDouble() * 255d) / 255f,
+                  b = (float)(prng.NextDouble() * 255d) / 255f;
+            return new Color(r, g, b, 1);
         }
 
         /// <summary>
@@ -79,8 +136,8 @@ namespace SPToolkits.Debugging
         private void OnDrawGizmos()
         {
             foreach (string id in Instance._ids)
-                if (_drawCalls.ContainsKey(id))
-                    _drawCalls[id]?.Invoke();
+                if (Instance._drawCalls.ContainsKey(id))
+                    Instance._drawCalls[id]?.Invoke();
         }
 #endif
     }
